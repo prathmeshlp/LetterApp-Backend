21985491592bc049d7daa8aa633dd35a9fba5f18
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2025-03-07 16:42:34 +0530

    Fix TypeScript error for accessType in passport config

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,19 +1,6 @@
-// declare module 'passport-google-oauth20' {
-//   // Default export as Strategy class
-//   class Strategy {
-//     constructor(
-//       options: StrategyOptions,
-//       verify: (accessToken: string, refreshToken: string | undefined, profile: any, done: (err: any, user?: any) => void) => void
-//     );
-//   }
-//   export = Strategy; // Define as default export
-
-//   interface StrategyOptions {
-//     clientID: string;
-//     clientSecret: string;
-//     callbackURL: string;
-//     accessType?: 'online' | 'offline';
-//     prompt?: string;
-//     scope?: string[];
-//   }
-// }
+declare module 'passport-google-oauth20' {
+    interface StrategyOptions {
+      accessType?: 'online' | 'offline';
+      prompt?: string;
+    }
+  }
